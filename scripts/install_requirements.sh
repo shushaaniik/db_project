05c92c5d9166e1ef01745a4154e221cc67e2cb49
shushaaniik <shushaniktovm@gmail.com>
2023-12-05 22:35:44 +0400

    added script for requirements installation, added db initializer and deleter

---
+++ scripts/install_requirements.sh
@@ -0,0 +1,3 @@
+sudo apt-get install libpq-dev
+pip3 install psycopg2-binary
+pip3 install sqlalchemy
---
+++ work_with_db/init_db.py
@@ -0,0 +1,44 @@
+import psycopg2
+from psycopg2 import sql
+
+
+def initialize_db(db_name, owner_username, owner_password="secret"):
+    # Connect to the default database to create a new DB
+    connection = psycopg2.connect(
+        dbname="postgres",
+        user="admin",
+        password="secret",
+        host="localhost",
+        port="5432"
+    )
+
+    connection.autocommit = True
+    cursor = connection.cursor()
+
+    try:
+        # Create a new DB
+        cursor.execute(sql.SQL(f"CREATE DATABASE {db_name}"))
+
+        connection.close()
+        connection = psycopg2.connect(
+            dbname=db_name,
+            user=owner_username,
+            password=owner_password,
+            host="localhost",
+            port="5432"
+        )
+        cursor = connection.cursor()
+
+        print(f"Database '{db_name}' created and owned by '{owner_username}'.")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+
+if __name__ == "__main__":
+    initialize_db(db_name="ConnectionOperators",
+                  owner_username="admin",
+                  owner_password="secret")
