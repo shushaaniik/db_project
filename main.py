513948c2316dcd585ef1901d9804a97896b81a83
shushanik <shushaniktovm@gmail.com>
2023-12-27 12:57:14 +0400

    updated main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.database_setup import engine
 from routers.connection_operator import router as connection_operator_router
 from routers.subscriber import router as subscriber_router
 from routers.connection import router as connection_router
+from routers.queries import router as query_router
 
 
 tables.Base.metadata.create_all(bind=engine)
@@ -13,3 +14,4 @@ app = FastAPI()
 app.include_router(router=connection_operator_router, prefix="/connection-operator")
 app.include_router(router=subscriber_router, prefix="/subscriber")
 app.include_router(router=connection_router, prefix="/connection")
+app.include_router(router=query_router, prefix="/queries")
